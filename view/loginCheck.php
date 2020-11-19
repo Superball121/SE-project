0bafe373a0dd608ca2928ef6b4f5c8d2185dc83f
DESKTOP-EB45E6D\cindy <s107213045@mail1.ncnu.edu.tw>
2020-11-19 14:57:06 +0800

    add many files

---
+++ view/loginCheck.php
@@ -0,0 +1,23 @@
+<?php
+session_start();
+require("../model/userModel.php");
+
+$userName = $_POST['id'];
+$passWord = $_POST['pwd'];
+
+if (checkUserIDPwd($userName, $passWord)) {
+  if($_SESSION['uID']=='student'){
+    header("Location: stuView.php");
+  }else if($_SESSION['uID']=='teacher'){
+    header("Location: teaView.php");
+  }else if($_SESSION['uID']=='secretary'){
+    header("Location: secView.php");
+  }else{
+    // header("Location: prinView.php");
+    // echo $userName;
+  }
+} else {
+	$_SESSION['uID']="";
+	header("Location: loginForm.php");
+}
+?>
